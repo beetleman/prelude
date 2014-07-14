7c713b40910d6adfcba1f806bf86f4e635679189
Mateusz Probachta <mateuszp@localhost.localdomain>
2014-07-14 11:42:50 +0200

    add key bind for change ispells dictionary

--- personal/beetleman.el
+++ personal/beetleman.el
@@ -140,6 +140,17 @@
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (add-to-list 'slime-contribs 'slime-fancy)
 
+;; ispell tweak
+(defun fd-switch-dictionary()
+  (interactive)
+  (let* ((dic ispell-current-dictionary)
+         (change (if (string= dic "polish") "english" "polish")))
+    (ispell-change-dictionary change)
+    (message "Dictionary switched from %s to %s" dic change)
+    ))
+
+(global-set-key (kbd "<f8>")   'fd-switch-dictionary)
+
 ;; server:
 (server-start)
 
