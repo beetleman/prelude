3ad7e29ea92b6ef2c9419ccef5847a9bf3869fc9
Mateusz Probachta <mateusz.probachta@gmail.com>
2014-08-24 20:09:57 +0200

    quickfix: missing require

--- personal/beetleman.el
+++ personal/beetleman.el
@@ -60,6 +60,7 @@
 (setq prelude-clean-whitespace-on-save nil)
 (setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
 
+(require 'moe-theme)
 (disable-theme 'zenburn)
 (load-theme 'moe-dark t)
 
