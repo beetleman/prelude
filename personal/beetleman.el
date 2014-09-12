1b80525bee6299f8fb2b9115cee60e90f36c02dc
beetleman <nigrantis.tigris@gmial.com>
2014-09-12 16:02:12 +0200

    diminish configuration

--- personal/beetleman.el
+++ personal/beetleman.el
@@ -56,9 +56,6 @@
 ;; set font:
 (add-to-list 'default-frame-alist '(font . "Source Code Pro-10"))
 
-;; set cursor color
-(add-to-list 'default-frame-alist '(cursor-color . "white"))
-
 (setq prelude-clean-whitespace-on-save nil)
 (setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
 
@@ -140,5 +137,36 @@
 
 ;; general tweak:
 (define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; diminish configuration
+(require 'diminish)
+(add-hook 'anaconda-mode-hook
+          (lambda ()
+            (diminish 'anaconda-mode)))
+(add-hook 'company-mode-hook
+          (lambda ()
+            (diminish 'company-mode)))
+(add-hook 'whitespace-mode-hook
+          (lambda ()
+            (diminish 'whitespace-mode)))
+(add-hook 'flyspell-mode-hook
+          (lambda ()
+            (diminish 'flyspell-mode)))
+(add-hook 'smartparens-mode-hook
+          (lambda ()
+            (diminish 'smartparens-mode)))
+(add-hook 'prelude-mode-hook
+          (lambda ()
+            (diminish 'prelude-mode)))
+(add-hook 'sphinx-doc-mode-hook
+          (lambda ()
+            (diminish 'sphinx-doc-mode)))
+(add-hook 'yas-minor-mode-hook
+          (lambda ()
+            (diminish 'yas-minor-mode)))
+(add-hook 'subword-mode-hook
+          (lambda ()
+            (diminish 'subword-mode)))
+
 
 ;;; beetleman.el ends here
