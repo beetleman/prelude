709b95189edb15f0609165fc5cf024f640acfa3c
beetleman <mateusz.probachta@gmail.com>
2015-01-21 13:46:55 +0100

    Enable `prelude-helm-everywhere` and fixing `anacoda-mode` (hack).

--- personal/20-python.el
+++ personal/20-python.el
@@ -1,5 +1,10 @@
 ;; python tweaks:
+(require 'sphinx-doc)
 (add-hook 'python-mode-hook (lambda ()
-                              (require 'sphinx-doc)
                               (sphinx-doc-mode t)))
+
+(add-hook 'anaconda-mode-hook (lambda ()
+                              (local-set-key (kbd "M-.") 'anaconda-mode-goto)
+                              (local-set-key (kbd "M-*") 'anaconda-nav-pop-marker)))
+
 (setq py-autopep8-options '("--max-line-length=400"))  ;; i like control this by my own
--- personal/30-projectile-prelude.el
+++ personal/30-projectile-prelude.el
@@ -1,9 +1,5 @@
 ;; projectile/prelude tweaks
 
-(require 'helm-projectile)
-(helm-projectile-on)
-(global-set-key (kbd "C-c h") 'helm-projectile)
-
 (setq projectile-enable-caching t)
 
 (global-set-key [C-menu] 'helm-imenu)
