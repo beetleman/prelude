d5f0ddcd6daaa22b02b4be932e86fcfcde3a9b85
beetleman <mateusz.probachta@gmail.com>
2015-06-01 16:20:30 +0200

    modify `anacoda-mode-goto`. Its work now with `decorators`.

--- personal/20-python.el
+++ personal/20-python.el
@@ -1,11 +1,25 @@
 ;; python tweaks:
 (require 'sphinx-doc)
-(add-hook 'python-mode-hook (lambda ()
-                              (highlight-indentation-mode t)
-                              (sphinx-doc-mode t)))
+(require 'anaconda-mode)
 
-(add-hook 'anaconda-mode-hook (lambda ()
-                              (local-set-key (kbd "M-.") 'anaconda-mode-goto)
-                              (local-set-key (kbd "M-*") 'anaconda-nav-pop-marker)))
+(add-hook 'python-mode-hook
+          (lambda ()
+            (highlight-indentation-mode t)
+            (sphinx-doc-mode t)))
+
+;; from anaconda-mode
+(defun my-anaconda-mode-goto ()
+  "Goto definition or fallback to assignment for thing at point."
+  (interactive)
+  (anaconda-nav-navigate
+   (or (anaconda-mode-call "goto_assignments")
+       (anaconda-mode-call "goto_definitions")
+       (error "No definition found"))
+   t))
+
+(add-hook 'anaconda-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-.") 'my-anaconda-mode-goto)
+            (local-set-key (kbd "M-*") 'anaconda-nav-pop-marker)))
 
 (setq py-autopep8-options '("--max-line-length=400"))  ;; i like control this by my own
