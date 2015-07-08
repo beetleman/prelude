94d7c344e3e94d15236e7b018b7fb25b6e035b6b
beetleman <mateusz.probachta@gmail.com>
2015-07-08 09:43:28 +0200

    conf `projectile-mode-line`

--- personal/30-projectile-prelude.el
+++ personal/30-projectile-prelude.el
@@ -5,4 +5,8 @@
 (global-set-key [C-menu] 'helm-imenu)
 (global-set-key (kbd "C-c h") 'helm-projectile)
 
+(custom-set-variables
+ '(projectile-mode-line
+   (quote
+    (:eval (format " {%s}" (projectile-project-name))))))
 ;; (setq prelude-guru nil)
