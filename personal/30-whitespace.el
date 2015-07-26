9028b20a1921ac1f69dcc8bc01653f58ba5089b3
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2015-07-26 14:26:27 +0200

    cleanup whitespace config

--- personal/30-whitespace.el
+++ personal/30-whitespace.el
@@ -1,2 +1,2 @@
-(setq prelude-clean-whitespace-on-save nil)
-(setq whitespace-style '(spaces tabs newline space-mark tab-mark newline-mark))
+;; (setq prelude-clean-whitespace-on-save nil)
+;  (setq whitespace-style '(spaces tabs newline space-mark tab-mark newline-mark))
