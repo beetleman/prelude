8e5a8ee05e90a09a182f7e50c2954d6268403546
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-04-02 22:48:11 +0200

    emmet in nxml mode

--- personal/20-web.el
+++ personal/20-web.el
@@ -15,6 +15,7 @@
 
 (require 'emmet-mode)
 
+(add-hook 'nxml-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode);; enable Emmet's css abbreviation.
 (add-hook 'scss-mode-hook  'emmet-mode)
